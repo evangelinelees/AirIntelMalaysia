4db1172092a9c126c82812a542e856718cd2bac2
evangelinelees <evangelinelees@gmail.com>
2026-07-20 17:21:43 +0800

    complete strip of middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,11 +1,5 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export function middleware() {
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
-};
